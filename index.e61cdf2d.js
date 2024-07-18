90515e9ebec2561f3938b93bce2cae9ef3ee0844
Olya <olyavidzi97@gmail.com>
2024-07-18 14:02:03 +0300

    make build

---
+++ index.e61cdf2d.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");n.appendChild(r),Object.keys(t).forEach(function(n){var a=document.createElement("li");a.textContent=n,r.appendChild(a),Object.keys(t[n]).length>0&&e(a,t[n])})}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.e61cdf2d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ulList = document.createElement("ul");
+    element.appendChild(ulList);
+    Object.keys(data).forEach((key)=>{
+        const liList = document.createElement("li");
+        liList.textContent = key;
+        ulList.appendChild(liList);
+        if (Object.keys(data[key]).length > 0) createTree(liList, data[key]);
+    });
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
